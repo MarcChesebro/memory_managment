446563a1ae77abb543b4d6b3694ff62fcc772514
Marcellus Chesebro <chesebma@eos11.cis.gvsu.edu>
2019-03-21 09:24:27 -0400

    started mapping

---
+++ memory_mapping.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define data1 16
+
+int main()
+{
+    char *heap1; 
+    int stack1 = 1;
+    int stack2 = 2;
+    int stack3 = 3;
+
+    heap1 = malloc (max);
+    free (data1);
+    return 0;
+}
