8432b85615cca5cd44a6d70713def77bf996c6c6
Marcellus Chesebro <chesebma@eos11.cis.gvsu.edu>
2019-03-21 09:15:13 -0400

    added first part of lab

---
+++ dynamic_input.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 16
+
+int main()
+{
+    char *data1; 
+    char next;
+    int i = 0;
+    int max = 16;
+
+    data1 = malloc (max);
+    printf ("Please input username: ");
+
+    // Grab first char
+    next = fgetc(stdin);
+
+    // grab chars until newline
+    while(next != '\n'){
+     
+	// if the char would overflow malloc double the size
+	if(!(i < max)){
+		max = max * 2;
+	    	data1 = realloc(data1, max);
+	}
+	
+	// put char in array
+	data1[i] = next;
+	i++;
+
+        // get next char
+	next = fgetc(stdin);
+    }
+    
+    // one last check for the null terminator
+    if(!(i < max)){
+	max = max * 2;
+    	data1 = realloc(data1, max);
+    }
+	    
+    data1[i] = '\0';
+
+    printf ("you entered: %s\n", data1);
+    free (data1);
+    return 0;
+}
